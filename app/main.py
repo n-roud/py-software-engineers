2af5dd5f1a7af9a47f4160cf7cfa4bfa437d75c4
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-11-04 20:10:31 +0200

    2 push

--- app/main.py
+++ app/main.py
@@ -45,10 +45,9 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = [
-            "Python", "SQL", "Django", "JavaScript", "HTML", "CSS"
-        ]
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
+        self.skills = BackendDeveloper(name).skills + FrontendDeveloper(name).skills
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
